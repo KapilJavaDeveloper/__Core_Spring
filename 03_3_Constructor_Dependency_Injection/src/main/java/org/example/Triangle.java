e8976144c834a6611b22fcf9bf8b22e9747f1e81
Kapil <kk7094948@gmail.com>
2026-05-26 12:04:03 +0530

    3 Constructor Dependency Injection

---
+++ 03_3_Constructor_Dependency_Injection/src/main/java/org/example/Triangle.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Triangle {
+    private String type ;
+    private int height ;
+
+    public Triangle(String type){
+        this.type = type ;
+    }
+    public Triangle(int height){
+        this.height = height ;
+    }
+    public Triangle(String type , int height){
+        this.type = type ;
+        this.height = height ;
+    }
+
+    // getters
+    public String getType(){
+        return this.type ;
+    }
+    public int getHeight(){
+        return this.height ;
+    }
+
+    public void draw(){
+        System.out.println(getType() + " triangle is drawn of height " + getHeight()) ;
+    }
+
+
+}
