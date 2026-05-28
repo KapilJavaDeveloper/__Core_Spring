ac96d60851999928ef2cd97514d1b3e2d9ffd393
Kapil <kk7094948@gmail.com>
2026-05-28 15:18:58 +0530

    Inner Beans and Alias

--- 05_inner_beans_alias/src/main/java/org/example/Main.java
+++ 05_inner_beans_alias/src/main/java/org/example/Main.java
@@ -1,16 +1,17 @@
 package org.example;
 
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Main Class started") ;
 
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml") ;
-        Triangle obj = (Triangle) context.getBean("triangle")  ;
+        Triangle obj = (Triangle) context.getBean("t") ;
         obj.draw() ;
-        System.out.println("Main Class") ;
 
 
     }
---
+++ 05_inner_beans_alias/src/main/java/org/example/Point.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Point {
+
+    private int X ;
+    private int Y ;
+
+    // setters
+    public void setX(int X){
+        this.X = X;
+    }
+    public void setY(int Y){
+        this.Y = Y ;
+    }
+
+    // getters
+    public int getX(){
+        return this.X ;
+    }
+    public int getY(){
+        return this.Y ;
+    }
+}
